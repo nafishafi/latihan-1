a0db0ffa7e2ad4641572e8fc2772f652ce486c46
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-04-08 13:17:54 +0700

    Refactor homepage layout and add Albums and Posts pages with basic structure

---
+++ app/posts/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./postPage.module.css"
+
+const Posts = () => {
+  return (
+    <div className={styles.bgRed}>
+      <h1>Posts Page</h1>
+    </div>
+  );
+}
+
+export default Posts;
