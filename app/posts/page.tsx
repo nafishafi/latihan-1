932bbc451d031e6203f5f8c93224f943f89bc7f5
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-04-08 16:12:39 +0700

    Add Posts page with dynamic content and user interaction components

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,10 +1,35 @@
-import styles from "./postPage.module.css"
+import CardList from "../components/Posts/CardList";
+import ViewUserButton from "../components/Posts/ViewUserButton";
 
-const Posts = () => {
+const base_url = "https://jsonplaceholder.typicode.com/posts";
+
+interface Iposts {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
+const Posts = async() => {
+  const response = await fetch(base_url)
+  const posts: Iposts[] = await response.json()
   return (
-    <div className={styles.bgRed}>
-      <h1>Posts Page</h1>
-    </div>
+    <>
+        <div className="bg-amber-500">
+        <h1>Posts Page</h1>
+        </div>
+        {posts.map((post) => {
+            return (
+                <CardList key={post.id}>
+                <p>{post.id}</p>
+                <i>{post.title}</i>
+                <p>{post.body}</p>
+                <ViewUserButton userId={post.userId}/>
+                </CardList>
+            )
+        })}
+
+    </>
   );
 }
 
